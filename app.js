d0db5b1d852c521b7dd5121fa59930f5fcd8c9ff
Darbaz Ali <darbaz.a.h@gmail.com>
2020-09-17 14:41:42 +0300

    setup session and passport

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const session = require('express-session');
+const passport = require('passport');
+
+
 
 const app = express();
 
@@ -7,6 +11,21 @@ const routes = require('./server/routes')
 // setup template engine
 app.set('views', './views');
 app.set('view engine', 'pug');
+
+/*==================================================
+    SETUP PASSPORT AND SESSION
+===================================================*/
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 // serve static files
 app.use(express.static(__dirname + '/public'));
